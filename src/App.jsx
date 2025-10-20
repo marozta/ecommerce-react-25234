e670fd0cea4d2bd421d71197db74daed0bceca88
Martin Rodriguez Ozta <marozta@gmail.com>
2025-10-20 18:47:46 -0300

    Agregado de Router a las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -6,7 +7,10 @@ function App() {
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='checkout' element={<div>test checkout page</div>} />
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+import App from './App';
+
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
